b2fb6080fdcb95d4a33109b6abc182a901c63a34
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-04-13 00:44:10 +0530

    Update forcesub.py

--- plugins/functions/forcesub.py
+++ plugins/functions/forcesub.py
@@ -15,7 +15,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/mavibot_support).",
                 parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_to_message_id=message.id,
@@ -39,7 +39,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/mavibot_support).",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_to_message_id=message.id,
